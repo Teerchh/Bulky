ce11a81733f6fa6f8f68791615a30bfe5b43816b
Teerch <teerchthegamer@gmail.com>
2026-08-15 10:13:08 +0100

    fix: ensure admin user is always seeded even if roles pre-exist

--- Bulky.DataAccess/DBInitializer/DBInitializer.cs
+++ Bulky.DataAccess/DBInitializer/DBInitializer.cs
@@ -14,25 +14,30 @@ public class DBInitializer(UserManager<IdentityUser> userManager, RoleManager<Id
         //migrations if they are not applied
         try
         {
-            if (db.Database.GetPendingMigrations().Count() > 0)
+            if (db.Database.GetPendingMigrations().Any())
             {
                 db.Database.Migrate();
             }
         }
-        catch (Exception ex) { }
+        catch (Exception) { }
 
-        //create roles if they are not created
+        //create roles individually if they are missing
         if (!roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
-        {
             roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
+        if (!roleManager.RoleExistsAsync(SD.Role_Employee).GetAwaiter().GetResult())
             roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+        if (!roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
             roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+        if (!roleManager.RoleExistsAsync(SD.Role_Company).GetAwaiter().GetResult())
             roleManager.CreateAsync(new IdentityRole(SD.Role_Company)).GetAwaiter().GetResult();
 
-            //if roles are not created, then we will create admin user as well.
-            //Credentials come from config: appsettings.Development.json locally, App Settings on Azure.
-            var adminEmail = configuration["Admin:Email"] ?? "admin@admin.com";
-            var adminPassword = configuration["Admin:Password"] ?? "Admin1234!";
+        //create admin user if it does not exist (independent of the roles above)
+        //Credentials come from config: appsettings.Development.json locally, App Settings on Azure.
+        var adminEmail = configuration["Admin:Email"] ?? "admin@admin.com";
+        var adminPassword = configuration["Admin:Password"] ?? "Admin1234!";
+        var adminUser = userManager.FindByEmailAsync(adminEmail).GetAwaiter().GetResult();
+        if (adminUser == null)
+        {
             var user = new ApplicationUser { UserName = adminEmail, Email = adminEmail, Name = "Administrator", PhoneNumber = "1234567890", StreetAddress = "admin av", State = "OYO", PostalCode = "200005", City = "IB" };
             userManager.CreateAsync(user, adminPassword).GetAwaiter().GetResult();
             userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
